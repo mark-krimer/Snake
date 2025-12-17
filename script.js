a2a88c4b89868643ef8adc58da1d3d1f72a3fca2
Mark Krimerman <mark.krimeran@gmail.com>
2025-12-17 18:46:22 -0500

    Snake movement & wall collisions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const canvasElem = document.getElementById("canvas");
 const canvas = canvasElem.getContext("2d");
 
+const body = document.querySelector("body");
+
 // Variables
 const scale = 50; // 18 * 50 = 900px
 const speed = 100; // ms delay between frames
@@ -10,7 +12,6 @@ let highScore = 0;
 let pixels = [];
 
 // Functions
-function gameLoop() {}
 
 function setupCanvas() {
 	canvasElem.width = 18 * scale;
@@ -21,28 +22,73 @@ function setupCanvas() {
 	canvasElem.style.imageRendering = "pixelated";
 }
 
-function drawPixel(x, y, width = 1, height = 1, colour = "black") {
-	let pixel = new Pixel(x, y, width, height, colour);
-	pixel.drawSquare();
+function drawPixel(x, y, width = 1, height = 1, colour = "black", type = "wall", merge = false) {
+	if (merge) {
+		const pixel = new Pixel(x, y, width, height, colour, type);
+		pixel.drawSquare();
+		return [pixel];
+	}
+
+	const created = [];
+	for (let i = 0; i < width; i++) {
+		for (let j = 0; j < height; j++) {
+			const px = x + i;
+			const py = y + j;
+			const pixel = new Pixel(px, py, 1, 1, colour, type);
+			pixel.drawSquare();
+			created.push(pixel);
+		}
+	}
+	return created;
 }
 
 function drawBoardStatics(borderColour = "black", playColour = "grey", scoreColour = "white") {
-	drawPixel(1, 1, 18, 18, borderColour);
-	drawPixel(2, 4, 16, 14, playColour);
-	drawPixel(2, 2, 5, 1, scoreColour);
-	drawPixel(13, 2, 5, 1, scoreColour);
+	drawPixel(1, 1, 18, 3, borderColour, "wall");
+	drawPixel(1, 4, 1, 15, borderColour, "wall");
+	drawPixel(18, 4, 1, 15, borderColour, "wall");
+	drawPixel(2, 18, 16, 1, borderColour, "wall");
+	drawPixel(2, 4, 16, 14, playColour, "board");
+	drawPixel(2, 2, 5, 1, scoreColour, "wall");
+	drawPixel(13, 2, 5, 1, scoreColour, "wall");
 }
 
-// Classes
+function checkMovementCompatibility(direction, nextDirection) {
+	if (direction == "right" && nextDirection == "left") {
+		return 1;
+	}
+	if (direction == "left" && nextDirection == "right") {
+		return 1;
+	}
+	if (direction == "up" && nextDirection == "down") {
+		return 1;
+	}
+	if (direction == "down" && nextDirection == "up") {
+		return 1;
+	}
+}
+
+function findPixel(x, y) {
+	for (let i = 0; i < pixels.length; i++) {
+		const pixel = pixels[i];
+		if (pixel.x === x && pixel.y === y) {
+			return pixel;
+		}
+	}
+	return undefined;
+}
+
 class Pixel {
-	constructor(x, y, width = 1, height = 1, colour = "black") {
+	constructor(x, y, width = 1, height = 1, colour = "black", type = "wall", addToPixels = true) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
 		this.colour = colour;
+		this.type = type;
 
-		pixels.push(this);
+		if (addToPixels) {
+			pixels.push(this);
+		}
 	}
 
 	drawSquare = function () {
@@ -53,19 +99,74 @@ class Pixel {
 
 class Snake {
 	constructor() {
-		this.segments = [new Pixel(7, 5, 1, 1, "green"), new Pixel(6, 5, 1, 1, "green"), new Pixel(5, 5, 1, 1, "green")];
+		this.segments = [
+			new Pixel(7, 5, 1, 1, "green", "snake", false), // head
+			new Pixel(6, 5, 1, 1, "green", "snake", false),
+			new Pixel(5, 5, 1, 1, "green", "snake", false),
+			new Pixel(4, 5, 1, 1, "green", "snake", false), // tail
+		];
 		this.direction = "right";
 		this.nextDirection = "right";
 
 		console.log("created");
 	}
 
-	drawSnake = function () {
-		console.log("drawing");
-
+	draw = function () {
 		this.segments.forEach((segment) => {
 			segment.drawSquare();
 		});
+	};
+
+	move = function () {
+		let newHeadX = this.segments[0].x;
+		let newHeadY = this.segments[0].y;
+
+		// Setting direction
+		if (checkMovementCompatibility(this.direction, this.nextDirection) == 1) {
+			this.nextDirection = this.direction;
+		}
+
+		this.direction = this.nextDirection;
+
+		// Remove tail
+		this.segments.splice(-1, 1);
+
+		// Add head
+		if (this.direction == "right") {
+			newHeadX++;
+		} else if (this.direction == "left") {
+			newHeadX--;
+		} else if (this.direction == "down") {
+			newHeadY++;
+		} else if (this.direction == "up") {
+			newHeadY--;
+		}
+
+		this.segments.splice(0, 0, new Pixel(newHeadX, newHeadY, 1, 1, "green", "snake"));
+
+		// Check collision
+		this.checkCollisions();
+	};
+
+	checkCollisions = function () {
+		// Finding head location
+		let headX = this.segments[0].x;
+		let headY = this.segments[0].y;
+
+		// Determining contact pixel
+		let contactPixel = findPixel(headX, headY);
+
+		if (contactPixel == undefined) {
+			return;
+		}
+
+		// Wall
+		console.log(contactPixel);
+		if (contactPixel.type == "wall") {
+			alert("lose");
+		}
+
+		// Apple
 	};
 }
 
@@ -84,15 +185,20 @@ setInterval(() => {
 	canvas.clearRect(0, 0, canvasElem.width, canvasElem.height);
 	pixels = [];
 
-	// Draw current score on the screen
-	// Move snake in current direction
-	// Collisions
-	// Apple
 	// Draw border
 	drawBoardStatics("darkslategrey", "greenyellow", "darkseagreen");
 
+	// Draw current score on the screen
+
+	// Move snake
+	player.move();
+
+	// Apple collisions
+
+	// Wall collisions
+
 	// Draw segments
-	player.drawSnake();
+	player.draw();
 
 	// Draw apple
 }, speed);
@@ -103,12 +209,28 @@ window.onload = function () {
 };
 
 // Player Inputs
+const directions = {
+	37: "left",
+	65: "left",
+	38: "up",
+	87: "up",
+	39: "right",
+	68: "right",
+	40: "down",
+	83: "down",
+};
+
+body.addEventListener("keydown", function (e) {
+	if (directions[e.keyCode] != undefined) {
+		player.nextDirection = directions[e.keyCode];
+	}
+});
 
 //todo		Fundementals
-//todo		 - Get snake moving
-//todo		 - Collisions (wall, self)
-//todo		 	- Track snake segment positions
-//todo		 	- Detect head position overlap
+//todo//		 - Get snake moving
+//todo//		 - Collisions (wall, self)
+//todo//		 	- Track snake segment positions
+//todo//		 	- Detect head position overlap
 //todo		 - Apple eating
 //todo		 	- Pick random unocupied apple position
 //todo		 	- Detect snake eating
